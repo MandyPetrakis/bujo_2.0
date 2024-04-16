3993cde2b406c0230ec09cd6d0db0daa9a02e34e
Mandy Petrakis <mandypetrakis@gmail.com>
2024-04-16 15:50:32 -0700

    seed files and db set up

---
+++ scripts/seed.js
@@ -0,0 +1,182 @@
+const { db } = require("@vercel/postgres");
+const {
+  users,
+  todos,
+  events,
+  habits,
+} = require("../src/app/lib/placeholder-data.js");
+const bcrypt = require("bcrypt");
+
+async function seedUsers(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // Create the "users" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      );
+    `;
+
+    console.log(`Created "users" table`);
+
+    // Insert data into the "users" table
+    const insertedUsers = await Promise.all(
+      users.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        return client.sql`
+        INSERT INTO users (id, name, email, password)
+        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+      })
+    );
+
+    console.log(`Seeded ${insertedUsers.length} users`);
+
+    return {
+      createTable,
+      users: insertedUsers,
+    };
+  } catch (error) {
+    console.error("Error seeding users:", error);
+    throw error;
+  }
+}
+
+async function seedTodos(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+    // Create the "todos" table if it doesn't exist
+    const createTable = await client.sql`
+    CREATE TABLE IF NOT EXISTS todos (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_id UUID NOT NULL,
+    description TEXT NOT NULL,
+    complete BOOLEAN NOT NULL,
+    due_date DATE,
+    completed_date DATE 
+  );
+`;
+
+    console.log(`Created "todos" table`);
+
+    // Insert data into the "todos" table
+    const insertedTodos = await Promise.all(
+      todos.map(
+        (todo) => client.sql`
+        INSERT INTO todos (user_id, description, complete, due_date, completed_date)
+        VALUES (${todo.user_id}, ${todo.description}, ${todo.complete}, ${todo.due_date}, ${todo.completed_date})
+        ON CONFLICT (id) DO NOTHING;
+      `
+      )
+    );
+
+    console.log(`Seeded ${insertedTodos.length} todos`);
+
+    return {
+      createTable,
+      todos: insertedTodos,
+    };
+  } catch (error) {
+    console.error("Error seeding todos:", error);
+    throw error;
+  }
+}
+
+async function seedEvents(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+    // Create the "customers" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS events (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        description TEXT NOT NULL,
+        user_id UUID NOT NULL,
+        date DATE NOT NULL
+      );
+    `;
+
+    console.log(`Created "events" table`);
+
+    // Insert data into the "customers" table
+    const insertedEvents = await Promise.all(
+      events.map(
+        (event) => client.sql`
+        INSERT INTO events (id, description, user_id, date)
+        VALUES (${event.id}, ${event.description}, ${event.user_id}, ${event.date})
+        ON CONFLICT (id) DO NOTHING;
+      `
+      )
+    );
+
+    console.log(`Seeded ${insertedEvents.length} events`);
+
+    return {
+      createTable,
+      events: insertedEvents,
+    };
+  } catch (error) {
+    console.error("Error seeding events:", error);
+    throw error;
+  }
+}
+
+async function seedHabits(client) {
+  try {
+    // Create the "habits" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS habits (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        description TEXT NOT NULL,
+        user_id UUID NOT NULL,
+        dates DATE[] NOT NULL
+      );
+    `;
+
+    console.log(`Created "habits" table`);
+
+    // Insert data into the "habits" table
+    const insertedHabits = await Promise.all(
+      habits.map(
+        (habit) => client.sql`
+        INSERT INTO habits (id, description, user_id, dates)
+        VALUES (${habit.id}, ${habit.description}, ${habit.user_id}, ${habit.dates})
+        ON CONFLICT (id) DO NOTHING;
+      `
+      )
+    );
+
+    console.log(`Seeded ${insertedHabits.length} habits`);
+
+    return {
+      createTable,
+      habits: insertedHabits,
+    };
+  } catch (error) {
+    console.error("Error seeding habits:", error);
+    throw error;
+  }
+}
+
+async function main() {
+  const client = await db.connect();
+
+  await seedUsers(client);
+  await seedTodos(client);
+  await seedEvents(client);
+  await seedHabits(client);
+
+  await client.end();
+}
+
+main().catch((err) => {
+  console.error(
+    "An error occurred while attempting to seed the database:",
+    err
+  );
+});
---
+++ src/app/lib/placeholder-data.js
@@ -0,0 +1,96 @@
+// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
+// https://nextjs.org/learn/dashboard-app/fetching-data
+const users = [
+  {
+    id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    name: "User",
+    email: "user@nextmail.com",
+    password: "123456",
+  },
+];
+
+const todos = [
+  {
+    id: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    description: "Give Fred a bath.",
+    complete: false,
+    due_date: "2024-04-01",
+    completed_date: null,
+  },
+  {
+    id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    description: "Groceries.",
+    complete: false,
+    due_date: "2024-04-06",
+    completed_date: null,
+  },
+  {
+    id: "3958dc9e-737f-4377-85e9-fec4b6a6442a",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    description: "Laundry",
+    complete: false,
+    due_date: "2024-04-06",
+    completed_date: null,
+  },
+  {
+    id: "50ca3e18-62cd-11ee-8c99-0242ac120002",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    description: "Go to the bank",
+    complete: false,
+    due_date: "2024-04-10",
+    completed_date: null,
+  },
+  {
+    id: "3958dc9e-787f-4377-85e9-fec4b6a6442a",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    description: "Wash the car",
+    complete: false,
+    due_date: "2024-04-15",
+    completed_date: null,
+  },
+];
+
+const events = [
+  {
+    id: "3958dc6e-787f-4377-85e9-fec4b6a6442a",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    description: "Carolyn's Birthday",
+    date: "2024-04-15",
+  },
+  {
+    id: "3958dc7e-787f-4377-85e9-fec4b6a6442a",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    description: "Ruby Event",
+    date: "2024-04-27",
+  },
+  {
+    id: "3958dc8e-787f-4377-85e9-fec4b6a6442a",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    description: "Park Hang",
+    date: "2024-04-20",
+  },
+];
+
+const habits = [
+  {
+    id: "3958dc0e-787f-4377-85e9-fec4b6a6442a",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    description: "Code",
+    dates: ["2024-04-02", "2024-04-01", "2024-04-03", "2024-04-06"],
+  },
+  {
+    id: "3958dc0e-787f-4377-85e9-fec4b6a6442a",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    description: "No Caf",
+    dates: ["2024-04-02", "2024-04-01", "2024-04-03", "2024-04-06"],
+  },
+];
+
+module.exports = {
+  users,
+  todos,
+  events,
+  habits,
+};
