1eed743b9aae5318a43cf50e001e3a70559c186a
Mandy Petrakis <mandypetrakis@gmail.com>
2024-04-21 14:55:44 -0700

    added keys

--- src/app/components/week-spread/habits.tsx
+++ src/app/components/week-spread/habits.tsx
@@ -10,14 +10,6 @@ export default async function Habits(props: {
 
   const habits = await fetchActiveHabits(year, month + 1);
 
-  const daysOfWeek = props.weekArray.map((day) => {
-    return (
-      <div className="mr-2 w-7 font-semibold place-content-center">
-        {day.dayOfWeek.charAt(0)}
-      </div>
-    );
-  });
-
   const habitRender = habits.map((habit) => {
     const week = props.weekArray.map((day) => {
       //convert dates_completed and day.date to string for comparison
@@ -28,7 +20,7 @@ export default async function Habits(props: {
       const completed = completedString.includes(date);
 
       return (
-        <div className="flex ">
+        <div key={day.day} className="flex ">
           <div
             key={day.day}
             className="border-t last:border-b border-l border-medium font-semibold h-[24px] w-[24px] flex place-content-center items-center"
@@ -39,7 +31,7 @@ export default async function Habits(props: {
       );
     });
     return (
-      <div className=" border-medium" key={habit.id}>
+      <div key={habit.id} className=" border-medium">
         <div className="-rotate-45 whitespace-nowrap w-[5px] text-xs">
           {habit.description}
         </div>
@@ -48,10 +40,5 @@ export default async function Habits(props: {
     );
   });
 
-  return (
-    <div className="flex pt-16">
-      <div className="pt-4">{daysOfWeek}</div>
-      {habitRender}
-    </div>
-  );
+  return <div className="flex pt-16">{habitRender}</div>;
 }
--- src/app/components/week-spread/tracking.tsx
+++ src/app/components/week-spread/tracking.tsx
@@ -5,12 +5,23 @@ export default async function Tracking(props: {
   startOfWeek: Date;
   weekArray: { dayOfWeek: string; day: number; month: string; date: Date }[];
 }) {
+  const daysOfWeek = props.weekArray.map((day) => {
+    return (
+      <div
+        key={day.day}
+        className="mr-2 w-7 font-semibold place-content-center"
+      >
+        {day.dayOfWeek.charAt(0)}
+      </div>
+    );
+  });
   return (
     <div className="border p-5 ml-5">
       <p className="flex place-content-center text-sm font-semibold mb-2">
         Tracking
       </p>
       <div className="flex">
+        <div className="pt-4">{daysOfWeek}</div>
         <Habits startOfWeek={props.startOfWeek} weekArray={props.weekArray} />
         <SleepTracker
           startOfWeek={props.startOfWeek}
